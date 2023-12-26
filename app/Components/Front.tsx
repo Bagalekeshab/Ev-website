3c085d8890c0524df4fcb7e204c96e6b5bcf09c0
KeshabBagale <ksubbagale@gmail.com>
2023-12-26 11:05:18 +0545

    first commit

---
+++ app/Components/Front.tsx
@@ -0,0 +1,14 @@
+
+const Front = () => {
+  return (
+    <div>
+     
+      <img className='w-full h-auto fixed' src="BYD.jpeg" alt="" />
+
+  
+      
+    </div>
+  )
+}
+
+export default Front
---
+++ app/Components/Page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Front from './Front'
+
+
+export default function Page() {
+  return (
+    <div>
+        <Navbar/>
+        <Front/>
+        
+
+      
+    </div>
+  )
+}
