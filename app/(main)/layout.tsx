9368594d930157d411e520eb0d5caa9e3a680d7d
KeshabBagale <ksubbagale@gmail.com>
2024-01-16 22:16:32 +0545

    add component

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -5,11 +5,17 @@ import Navbar from "@/components/header/Navbar"
 const MainLayout = ({children}:{children: React.ReactNode}) => {
   return (
         <>
+       <div className="layout flex flex-col justify-between">
+        <div>
         <Navbar/>
+        </div>
         <div>
             {children}
         </div>
+        <div>
         <Footer/>
+        </div>
+        </div>
         </>
   )
 }
--- app/tesla/page.tsx
+++ app/tesla/page.tsx
@@ -1,17 +1,50 @@
-
+import Footer from "@/components/footer/Footer"
+import Image from "next/image"
 
 const TeslaPage = () => {
   return (
-    <div>
-      <p>Tesla</p>
-      <div className="container">
-        <div className="box1">
-          
+    <>
+   <div>
+      <p className="text-center text-3xl bg-black text-white h-20 py-5">Tesla</p>
+      <div className="container m-auto ">
+        <div>
+          <img className="w-full h-auto p-10" src="/images/tesla_mode_s.jpg" alt="car" />
         </div>
+
+        <p className="text-center text-3xl font-bold h-20 py-6 underline">Models</p>
+
+        <div className="section grid grid-cols-2 px-2 py-5 md:grid-cols-4 pl-10">
+    
+        <div className="box1 border-2 w-44 h-52 flex flex-col ">
+          <img className="h-full w-auto object-cover " src="/images/tesla_mode_s.jpg" alt="car"/>
+          <button className="bg-red-900 text-white w-auto h-10">See more</button>
+          </div>
+
+          <div className="box1 border-2 w-44 h-52 flex flex-col ">
+          <img className="h-full w-auto object-cover " src="/images/tesla_mode_s.jpg" alt="car"/>
+          <button className="bg-red-900 text-white w-auto h-10">See more</button>
+          </div>
+          <div className="box1 border-2 w-44 h-52 flex flex-col ">
+          <img className="h-full w-auto object-cover " src="/images/tesla_mode_s.jpg" alt="car"/>
+          <button className="bg-red-900 text-white w-auto h-10">See more</button>
+          </div>
+          <div className="box1 border-2 w-44 h-52 flex flex-col ">
+          <img className="h-full w-auto object-cover " src="/images/tesla_mode_s.jpg" alt="car"/>
+          <button className="bg-red-900 text-white w-auto h-10">See more</button>
+          </div>
+
+         
+
+
+        </div>
+        
       </div>
       
       
     </div>
+
+    <Footer/> 
+    </>
   )
 }
 
